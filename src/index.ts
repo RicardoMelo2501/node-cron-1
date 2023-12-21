579bbcc1db13de123b98b0db72d7a18226b2d638
Ricardo Melo <83253741+RicardoMelo2501@users.noreply.github.com>
2023-12-20 23:45:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,17 @@ import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
+
+  // Substitua 'seu_script.php' pelo caminho do seu script PHP
+  const comandoPHP = 'php seu_script.php';
+
+  // Execute o script PHP usando o comando PHP
+  exec(comandoPHP, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erro ao executar o script PHP: ${error}`);
+      return;
+    }
+    console.log(`Saída do script PHP: ${stdout}`);
+    console.error(`Erros do script PHP: ${stderr}`);
+  });
 });
